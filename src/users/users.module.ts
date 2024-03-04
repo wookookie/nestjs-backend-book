6c2c6fa37d36ded882a202bcab3911e36ce73887
wookookie <wookookie404@gmail.com>
2024-03-04 20:52:03 +0900

    Create `usersModule`

---
+++ src/users/users.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
+import { EmailService } from "src/email/email.service";
+
+@Module({
+  imports: [],
+  controllers: [UsersController],
+  providers: [UsersService, EmailService],
+})
+export class UsersModule {}
