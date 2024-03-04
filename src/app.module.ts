c1253359bf6b066aa98861b5a2a008aa560f43aa
wookookie <wookookie404@gmail.com>
2024-03-04 16:29:11 +0900

    Remove demo controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { UsersModule } from "./users/users.module";
 import { ApiController } from "./api/api.controller";
 
 @Module({
   imports: [UsersModule],
-  controllers: [ApiController, AppController], // 1: ApiController(sub-domain) -> 2: AppController
-  providers: [AppService],
+  controllers: [ApiController], // ApiController(sub-domain)
+  providers: [],
 })
 export class AppModule {}
