f2dd805f9ad5f92f44b858952f416283e5a57968
wookookie <wookookie404@gmail.com>
2024-03-04 19:42:22 +0900

    Remove `UserModule`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from "@nestjs/common";
-import { UsersModule } from "./users/users.module";
 import { ApiController } from "./api/api.controller";
 
 @Module({
-  imports: [UsersModule],
+  imports: [],
   controllers: [ApiController], // ApiController(sub-domain)
   providers: [],
 })
