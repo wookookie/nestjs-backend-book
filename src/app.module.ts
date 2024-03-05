fee347d812dd2221f9d5a276fce0e2cd943a5878
wookookie <wookookie404@gmail.com>
2024-03-05 11:56:27 +0900

    Apply env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import emailConfig from "./config/email.config";
 import { UsersModule } from "./users/users.module";
+import { validationSchema } from "./config/validation.schema";
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { UsersModule } from "./users/users.module";
       envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
       load: [emailConfig],
       isGlobal: true,
+      validationSchema,
     }),
     UsersModule,
   ],
